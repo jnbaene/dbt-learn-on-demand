3778ac097bb620d2696be7731ae034eda66fc60f
Jose Baene <jose.baene@factored.ai>
2021-07-13 21:52:45 +0000

    MAterializations and fucntions

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,32 +1,35 @@
-with orders as (
-    select * from {{ref('stg_orders')}}
+{{ config (
+    materialized='ephemeral'
+)}}
+
+
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
 payments as (
-    select * from {{ref('stg_payments')}}
+    select * from {{ ref('stg_payments') }}
 ),
 
 order_payments as (
-
     select
         order_id,
         sum(case when status = 'success' then amount end) as amount
 
     from payments
-    group by order_id
+    group by 1
 ),
 
 final as (
 
     select
-        orders.customer_id,
         orders.order_id,
-        order_payments.amount,
-        orders.order_date
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
 
     from orders
     left join order_payments using (order_id)
-
 )
 
 select * from final
